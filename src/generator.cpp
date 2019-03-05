162efb0b279f74f41ad9a2aed3c30d6b7b22212b
Xuan-Huy Cao <xuan-huy.cao@outlook.com>
2019-03-05 11:55:24 +0100

    add generator

--- src/generator.cpp
+++ src/generator.cpp
@@ -1,11 +1,24 @@
 #pragma once
 
-class Generator : public gram1BaseVisitor {
+class Generator : public PLDCompBaseVisitor {
 public:
-    Generator(File * f): fp(f) {}
-    antlrcpp:Any visitProg(gram1Parser::ProgContext *ctx) override {
-        
+    Generator(string filename): os(filename) {}
+    antlrcpp:Any visitProg(PLDComp::ProgContext *ctx) override {
+        os.write('.text\n.global main\n');
+        os.write('main:\n');
+        os.write('pushq %rbp\n');
+        os.write('movq %rsp, %rbp\n');
+        visit(ctx->Return_intruction());
+        os.write('popq %rbp\n');
+        os.write('ret \n');
+    }
+
+    antlrcpp::Any visitReturn_instruction(PLDCompParser::Return_instructionContext *ctx) override {
+        os.write('movl $');
+        os.write(ctx->INT()->getText());
+        os.write(", %eax\n");
+        return;
     }
 private: 
-    File * fp;
+    ofstream os;
 }
