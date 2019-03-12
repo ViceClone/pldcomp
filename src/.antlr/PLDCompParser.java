2124a359637f8692dbc80b2e23f75d53aba069b4
Amine M. Boulouma <amine@boulouma.com>
2019-03-12 12:18:40 +0100

    Cleaned code

--- src/.antlr/PLDCompParser.java
+++ src/.antlr/PLDCompParser.java
@@ -185,7 +185,7 @@ public class PLDCompParser extends Parser {
 		try {
 			enterOuterAlt(_localctx, 1);
 			{
-			setState(30); 
+			setState(32); 
 			_errHandler.sync(this);
 			_la = _input.LA(1);
 			do {
@@ -193,12 +193,14 @@ public class PLDCompParser extends Parser {
 				{
 				setState(29);
 				statement();
+				setState(30);
+				match(SEMICOLON);
 				}
 				}
-				setState(32); 
+				setState(34); 
 				_errHandler.sync(this);
 				_la = _input.LA(1);
-			} while ( _la==RETURN || _la==INT_TYPE );
+			} while ( (((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << RETURN) | (1L << INT_TYPE) | (1L << ID))) != 0) );
 			}
 		}
 		catch (RecognitionException re) {
@@ -219,6 +221,9 @@ public class PLDCompParser extends Parser {
 		public ReturnstatementContext returnstatement() {
 			return getRuleContext(ReturnstatementContext.class,0);
 		}
+		public AssignmentstatContext assignmentstat() {
+			return getRuleContext(AssignmentstatContext.class,0);
+		}
 		public StatementContext(ParserRuleContext parent, int invokingState) {
 			super(parent, invokingState);
 		}
@@ -229,21 +234,28 @@ public class PLDCompParser extends Parser {
 		StatementContext _localctx = new StatementContext(_ctx, getState());
 		enterRule(_localctx, 6, RULE_statement);
 		try {
-			setState(36);
+			setState(39);
 			_errHandler.sync(this);
 			switch (_input.LA(1)) {
 			case INT_TYPE:
 				enterOuterAlt(_localctx, 1);
 				{
-				setState(34);
+				setState(36);
 				vardeclaration();
 				}
 				break;
 			case RETURN:
 				enterOuterAlt(_localctx, 2);
 				{
-				setState(35);
+				setState(37);
 				returnstatement();
+				}
+				break;
+			case ID:
+				enterOuterAlt(_localctx, 3);
+				{
+				setState(38);
+				assignmentstat();
 				}
 				break;
 			default:
@@ -279,14 +291,22 @@ public class PLDCompParser extends Parser {
 		public TerminalNode ID() { return getToken(PLDCompParser.ID, 0); }
 		public DeclWithoutAssignmentContext(VardeclarationContext ctx) { copyFrom(ctx); }
 	}
+	public static class DeclWithAssignmentIDContext extends VardeclarationContext {
+		public TypeContext type() {
+			return getRuleContext(TypeContext.class,0);
+		}
+		public List<TerminalNode> ID() { return getTokens(PLDCompParser.ID); }
+		public TerminalNode ID(int i) {
+			return getToken(PLDCompParser.ID, i);
+		}
+		public DeclWithAssignmentIDContext(VardeclarationContext ctx) { copyFrom(ctx); }
+	}
 	public static class DeclWithAssignmentContext extends VardeclarationContext {
 		public TypeContext type() {
 			return getRuleContext(TypeContext.class,0);
 		}
 		public TerminalNode ID() { return getToken(PLDCompParser.ID, 0); }
-		public ExprContext expr() {
-			return getRuleContext(ExprContext.class,0);
-		}
+		public TerminalNode INT() { return getToken(PLDCompParser.INT, 0); }
 		public DeclWithAssignmentContext(VardeclarationContext ctx) { copyFrom(ctx); }
 	}
 
@@ -294,35 +314,45 @@ public class PLDCompParser extends Parser {
 		VardeclarationContext _localctx = new VardeclarationContext(_ctx, getState());
 		enterRule(_localctx, 8, RULE_vardeclaration);
 		try {
-			setState(48);
+			setState(54);
 			_errHandler.sync(this);
 			switch ( getInterpreter().adaptivePredict(_input,2,_ctx) ) {
 			case 1:
 				_localctx = new DeclWithoutAssignmentContext(_localctx);
 				enterOuterAlt(_localctx, 1);
 				{
-				setState(38);
+				setState(41);
 				type();
-				setState(39);
+				setState(42);
 				match(ID);
-				setState(40);
-				match(SEMICOLON);
 				}
 				break;
 			case 2:
 				_localctx = new DeclWithAssignmentContext(_localctx);
 				enterOuterAlt(_localctx, 2);
 				{
-				setState(42);
-				type();
-				setState(43);
-				match(ID);
 				setState(44);
-				match(ASSIGNMENT);
+				type();
 				setState(45);
-				expr();
+				match(ID);
 				setState(46);
-				match(SEMICOLON);
+				match(ASSIGNMENT);
+				setState(47);
+				match(INT);
+				}
+				break;
+			case 3:
+				_localctx = new DeclWithAssignmentIDContext(_localctx);
+				enterOuterAlt(_localctx, 3);
+				{
+				setState(49);
+				type();
+				setState(50);
+				match(ID);
+				setState(51);
+				match(ASSIGNMENT);
+				setState(52);
+				match(ID);
 				}
 				break;
 			}
@@ -339,28 +369,60 @@ public class PLDCompParser extends Parser {
 	}
 
 	public static class AssignmentstatContext extends ParserRuleContext {
-		public TerminalNode ID() { return getToken(PLDCompParser.ID, 0); }
-		public ExprContext expr() {
-			return getRuleContext(ExprContext.class,0);
-		}
 		public AssignmentstatContext(ParserRuleContext parent, int invokingState) {
 			super(parent, invokingState);
 		}
 		@Override public int getRuleIndex() { return RULE_assignmentstat; }
+	 
+		public AssignmentstatContext() { }
+		public void copyFrom(AssignmentstatContext ctx) {
+			super.copyFrom(ctx);
+		}
+	}
+	public static class AssignmentIDContext extends AssignmentstatContext {
+		public List<TerminalNode> ID() { return getTokens(PLDCompParser.ID); }
+		public TerminalNode ID(int i) {
+			return getToken(PLDCompParser.ID, i);
+		}
+		public AssignmentIDContext(AssignmentstatContext ctx) { copyFrom(ctx); }
+	}
+	public static class AssignmentINTContext extends AssignmentstatContext {
+		public TerminalNode ID() { return getToken(PLDCompParser.ID, 0); }
+		public TerminalNode INT() { return getToken(PLDCompParser.INT, 0); }
+		public AssignmentINTContext(AssignmentstatContext ctx) { copyFrom(ctx); }
 	}
 
 	public final AssignmentstatContext assignmentstat() throws RecognitionException {
 		AssignmentstatContext _localctx = new AssignmentstatContext(_ctx, getState());
 		enterRule(_localctx, 10, RULE_assignmentstat);
 		try {
-			enterOuterAlt(_localctx, 1);
-			{
-			setState(50);
-			match(ID);
-			setState(51);
-			match(ASSIGNMENT);
-			setState(52);
-			expr();
+			setState(62);
+			_errHandler.sync(this);
+			switch ( getInterpreter().adaptivePredict(_input,3,_ctx) ) {
+			case 1:
+				_localctx = new AssignmentINTContext(_localctx);
+				enterOuterAlt(_localctx, 1);
+				{
+				setState(56);
+				match(ID);
+				setState(57);
+				match(ASSIGNMENT);
+				setState(58);
+				match(INT);
+				}
+				break;
+			case 2:
+				_localctx = new AssignmentIDContext(_localctx);
+				enterOuterAlt(_localctx, 2);
+				{
+				setState(59);
+				match(ID);
+				setState(60);
+				match(ASSIGNMENT);
+				setState(61);
+				match(ID);
+				}
+				break;
 			}
 		}
 		catch (RecognitionException re) {
@@ -390,11 +452,11 @@ public class PLDCompParser extends Parser {
 		try {
 			enterOuterAlt(_localctx, 1);
 			{
-			setState(54);
+			setState(64);
 			match(RETURN);
-			setState(55);
+			setState(65);
 			expr();
-			setState(56);
+			setState(66);
 			match(SEMICOLON);
 			}
 		}
@@ -439,14 +501,14 @@ public class PLDCompParser extends Parser {
 		ExprContext _localctx = new ExprContext(_ctx, getState());
 		enterRule(_localctx, 14, RULE_expr);
 		try {
-			setState(64);
+			setState(74);
 			_errHandler.sync(this);
 			switch (_input.LA(1)) {
 			case INT:
 				_localctx = new ConstContext(_localctx);
 				enterOuterAlt(_localctx, 1);
 				{
-				setState(58);
+				setState(68);
 				match(INT);
 				}
 				break;
@@ -454,7 +516,7 @@ public class PLDCompParser extends Parser {
 				_localctx = new VarContext(_localctx);
 				enterOuterAlt(_localctx, 2);
 				{
-				setState(59);
+				setState(69);
 				match(ID);
 				}
 				break;
@@ -462,11 +524,11 @@ public class PLDCompParser extends Parser {
 				_localctx = new ParContext(_localctx);
 				enterOuterAlt(_localctx, 3);
 				{
-				setState(60);
+				setState(70);
 				match(LEFT_PARENTHESE);
-				setState(61);
+				setState(71);
 				expr();
-				setState(62);
+				setState(72);
 				match(RIGHT_PARENTHESE);
 				}
 				break;
@@ -498,7 +560,7 @@ public class PLDCompParser extends Parser {
 		try {
 			enterOuterAlt(_localctx, 1);
 			{
-			setState(66);
+			setState(76);
 			match(INT_TYPE);
 			}
 		}
@@ -514,23 +576,25 @@ public class PLDCompParser extends Parser {
 	}
 
 	public static final String _serializedATN =
-		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\16G\4\2\t\2\4\3\t"+
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\16Q\4\2\t\2\4\3\t"+
 		"\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\4\b\t\b\4\t\t\t\4\n\t\n\3\2\3\2\3\2"+
-		"\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\4\6\4!\n\4\r\4\16\4\"\3\5\3\5\5\5\'"+
-		"\n\5\3\6\3\6\3\6\3\6\3\6\3\6\3\6\3\6\3\6\3\6\5\6\63\n\6\3\7\3\7\3\7\3"+
-		"\7\3\b\3\b\3\b\3\b\3\t\3\t\3\t\3\t\3\t\3\t\5\tC\n\t\3\n\3\n\3\n\2\2\13"+
-		"\2\4\6\b\n\f\16\20\22\2\2\2B\2\24\3\2\2\2\4\27\3\2\2\2\6 \3\2\2\2\b&\3"+
-		"\2\2\2\n\62\3\2\2\2\f\64\3\2\2\2\168\3\2\2\2\20B\3\2\2\2\22D\3\2\2\2\24"+
-		"\25\5\4\3\2\25\26\7\2\2\3\26\3\3\2\2\2\27\30\5\22\n\2\30\31\7\5\2\2\31"+
-		"\32\7\13\2\2\32\33\7\f\2\2\33\34\7\n\2\2\34\35\5\6\4\2\35\36\7\t\2\2\36"+
-		"\5\3\2\2\2\37!\5\b\5\2 \37\3\2\2\2!\"\3\2\2\2\" \3\2\2\2\"#\3\2\2\2#\7"+
-		"\3\2\2\2$\'\5\n\6\2%\'\5\16\b\2&$\3\2\2\2&%\3\2\2\2\'\t\3\2\2\2()\5\22"+
-		"\n\2)*\7\5\2\2*+\7\b\2\2+\63\3\2\2\2,-\5\22\n\2-.\7\5\2\2./\7\7\2\2/\60"+
-		"\5\20\t\2\60\61\7\b\2\2\61\63\3\2\2\2\62(\3\2\2\2\62,\3\2\2\2\63\13\3"+
-		"\2\2\2\64\65\7\5\2\2\65\66\7\7\2\2\66\67\5\20\t\2\67\r\3\2\2\289\7\3\2"+
-		"\29:\5\20\t\2:;\7\b\2\2;\17\3\2\2\2<C\7\6\2\2=C\7\5\2\2>?\7\13\2\2?@\5"+
-		"\20\t\2@A\7\f\2\2AC\3\2\2\2B<\3\2\2\2B=\3\2\2\2B>\3\2\2\2C\21\3\2\2\2"+
-		"DE\7\4\2\2E\23\3\2\2\2\6\"&\62B";
+		"\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\4\3\4\3\4\6\4#\n\4\r\4\16\4$\3\5\3"+
+		"\5\3\5\5\5*\n\5\3\6\3\6\3\6\3\6\3\6\3\6\3\6\3\6\3\6\3\6\3\6\3\6\3\6\5"+
+		"\69\n\6\3\7\3\7\3\7\3\7\3\7\3\7\5\7A\n\7\3\b\3\b\3\b\3\b\3\t\3\t\3\t\3"+
+		"\t\3\t\3\t\5\tM\n\t\3\n\3\n\3\n\2\2\13\2\4\6\b\n\f\16\20\22\2\2\2O\2\24"+
+		"\3\2\2\2\4\27\3\2\2\2\6\"\3\2\2\2\b)\3\2\2\2\n8\3\2\2\2\f@\3\2\2\2\16"+
+		"B\3\2\2\2\20L\3\2\2\2\22N\3\2\2\2\24\25\5\4\3\2\25\26\7\2\2\3\26\3\3\2"+
+		"\2\2\27\30\5\22\n\2\30\31\7\5\2\2\31\32\7\13\2\2\32\33\7\f\2\2\33\34\7"+
+		"\n\2\2\34\35\5\6\4\2\35\36\7\t\2\2\36\5\3\2\2\2\37 \5\b\5\2 !\7\b\2\2"+
+		"!#\3\2\2\2\"\37\3\2\2\2#$\3\2\2\2$\"\3\2\2\2$%\3\2\2\2%\7\3\2\2\2&*\5"+
+		"\n\6\2\'*\5\16\b\2(*\5\f\7\2)&\3\2\2\2)\'\3\2\2\2)(\3\2\2\2*\t\3\2\2\2"+
+		"+,\5\22\n\2,-\7\5\2\2-9\3\2\2\2./\5\22\n\2/\60\7\5\2\2\60\61\7\7\2\2\61"+
+		"\62\7\6\2\2\629\3\2\2\2\63\64\5\22\n\2\64\65\7\5\2\2\65\66\7\7\2\2\66"+
+		"\67\7\5\2\2\679\3\2\2\28+\3\2\2\28.\3\2\2\28\63\3\2\2\29\13\3\2\2\2:;"+
+		"\7\5\2\2;<\7\7\2\2<A\7\6\2\2=>\7\5\2\2>?\7\7\2\2?A\7\5\2\2@:\3\2\2\2@"+
+		"=\3\2\2\2A\r\3\2\2\2BC\7\3\2\2CD\5\20\t\2DE\7\b\2\2E\17\3\2\2\2FM\7\6"+
+		"\2\2GM\7\5\2\2HI\7\13\2\2IJ\5\20\t\2JK\7\f\2\2KM\3\2\2\2LF\3\2\2\2LG\3"+
+		"\2\2\2LH\3\2\2\2M\21\3\2\2\2NO\7\4\2\2O\23\3\2\2\2\7$)8@L";
 	public static final ATN _ATN =
 		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
 	static {
