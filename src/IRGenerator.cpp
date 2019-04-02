e9a6245db5e1d88fe4c227d60358e49c0a349585
Xuan-Huy Cao <xuan-huy.cao@outlook.com>
2019-04-02 10:05:33 +0200

    ir

--- src/IRGenerator.cpp
+++ src/IRGenerator.cpp
@@ -30,9 +30,27 @@ antlrcpp::Any IRGenerator::visitReturnstatement(PLDCompParser::ReturnstatementCo
     return var;
 }
 
+antlrcpp::Any IRGenerator::visitConst(PLDCompParser::ConstContext *ctx) {
+    string var = current_cfg->create_new_tempvar(Int);
+    vector<string> params = {var,ctx->INT()->getText()};
+    current_cfg->current_bb->add_IRInstr(IRInstr::ldconst,Int,params);
+    return var;
+}
+
+antlrcpp::Any IRGenerator::visitNegConst(PLDCompParser::NegConstContext *ctx) {
+    string var = current_cfg->create_new_tempvar(Int);
+    vector<string> params = {var,("-"+ctx->INT()->getText())};
+    current_cfg->current_bb->add_IRInstr(IRInstr::ldconst,Int,params);
+    return var;
+}
+
 antlrcpp::Any IRGenerator::visitPar(PLDCompParser::ParContext *ctx) {
     string var = visit(ctx->expr());
     return var;
+}
+
+antlrcpp::Any IRGenerator::visitVar(PLDCompParser::VarContext *ctx) {
+    return (string)(ctx->ID()->getText());
 }
 
 antlrcpp::Any IRGenerator::visitMultiplicativeOp(PLDCompParser::MultiplicativeOpContext *ctx) {
@@ -43,6 +61,21 @@ antlrcpp::Any IRGenerator::visitMultiplicativeOp(PLDCompParser::MultiplicativeOp
     IRInstr::Operation op;
     if (ctx->op->getText().compare("*") == 0) {
         op = IRInstr::mul;
+    }
+    current_cfg->current_bb->add_IRInstr(op,Int,params);
+    return var3;
+}
+
+antlrcpp::Any IRGenerator::visitAdditiveOp(PLDCompParser::AdditiveOpContext *ctx) {
+    string var1 = visit(ctx->expr(0));
+    string var2 = visit(ctx->expr(1));
+    string var3 = current_cfg->create_new_tempvar(Int);
+    vector<string> params = {var3, var1, var2};
+    IRInstr::Operation op;
+    if (ctx->op->getText().compare("+") == 0) {
+        op = IRInstr::add;
+    } else if (ctx->op->getText().compare("-") == 0) {
+        op = IRInstr::sub;
     }
     current_cfg->current_bb->add_IRInstr(op,Int,params);
     return var3;
