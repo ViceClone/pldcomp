eaa88bd5c71aa51c289a1ed03f1832c83e52f10e
Amine M. Boulouma <amine@boulouma.com>
2019-04-12 09:55:31 +0200

    Uupdates demo tests

---
+++ test/demo_test/if_and_while.c
@@ -0,0 +1,10 @@
+int main() {
+    int i = 0;
+    int j = 0;
+    if(i < 10){
+        while(j < 10){
+            j++;
+        }
+        i++;
+    }
+}
---
+++ test/demo_test/while_and_if.c
@@ -0,0 +1,10 @@
+int main() {
+    int i = 0;
+    int j = 0;
+    while(i < 10){
+        if(j < 10){
+            j++;
+        }
+        i++;
+    }
+}
